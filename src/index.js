edfe5a4649b4b7f3478e1f736cb974d84bd019ff
walkedSnail <17701309202@163.com>
2019-04-26 10:18:28 +0800

    asset management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './style.css'
+import Icon from './icon.png'
+import Data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = 'Hello webpack';
     element.classList.add('hello');
+
+    // Add the image to our existing div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+    console.log(Data);
 
     return element;
 }
